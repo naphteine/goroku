acaf0be585507308799998f4360eb9a12c3fccdb
naphteine <58526116+naphteine@users.noreply.github.com>
2022-11-06 12:51:54 +0300

    Uploaded example secrets.go

---
+++ secrets.go
@@ -0,0 +1,23 @@
+package main
+
+const (
+	Instance          = "Goroku"
+	dbHost            = "localhost"
+	dbPort            = 5432
+	dbUser            = "root"
+	dbPasswd          = "password"
+	dbName            = "goroku"
+	urlLogin          = "/login"
+	urlLogout         = "/logout"
+	urlPostLogin      = "/post/login"
+	urlRegister       = "/signup"
+	urlPostRegister   = "/post/signup"
+	tmplLogin         = "templates/login.html"
+	tmplRegister      = "templates/signup.html"
+	urlCaption        = "/c"
+	urlCaptionCreate  = "/caption"
+	urlPostCaption    = "/post/caption"
+	tmplCaption       = "templates/c.html"
+	tmplCaptionCreate = "templates/caption.html"
+	urlPostEntry      = "/post/entry"
+)
